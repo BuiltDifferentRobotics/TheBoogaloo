c36fc0bf7e7bed51a8d45c5a76ca3a6ca34f260b
sharvilgarg <sharvil.garg@gmail.com>
2021-02-18 20:37:27 -0600

    Added basic code

---
+++ FreeRTOS_testing/Blink/src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <FreeRTOS.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LED1 2
+#define LED2 4
+
+void Blink1(void *params) {
+ pinMode(LED1, OUTPUT);
+ int i=0;
+ while (1)
+ {
+     digitalWrite(LED1, HIGH);
+     vTaskDelay(1000);
+     digitalWrite(LED1, LOW);
+     vTaskDelay(1000);
+    
+ }
+ 
+}
+void Blink2(void *params){
+ pinMode(LED2, OUTPUT);
+ int i=0;
+ while (1)
+ {
+     digitalWrite(LED2, HIGH);
+     vTaskDelay(500);
+     digitalWrite(LED2, LOW);
+     vTaskDelay(500);
+ }
+ 
+}
+void main() {
+ Serial.begin(115200);
+ xTaskCreate(Blink1, "Task1", 128, NULL, 1, NULL);
+ xTaskCreate(Blink2, "Task2", 128, NULL, 1, NULL);
+}
