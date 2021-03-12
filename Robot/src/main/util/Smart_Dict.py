26695b5e176f6551db1db8b5575915d8f12cd461
Explicious <46688743+Explicious@users.noreply.github.com>
2021-03-11 19:56:59 -0600

    Added scipy spline interpolation

---
+++ Robot/src/main/util/Smart_Dict.py
@@ -0,0 +1,31 @@
+
+from scipy.interpolate import UnivariateSpline
+
+class Smart_Dict():
+
+    values = {} # (int x, int y)
+
+    interpol_range = 0
+    top_bound = (0,0) #(max x, bound y)
+    bot_bound = (0,0) #(min x, bound y)
+
+    def __init__(self):
+        pass
+
+    def get(self, x):
+        try:
+            out = self.values[x]
+            return out
+        except KeyError:
+            out = self.interpolate(x)
+            return out
+
+    def interpolate(self, x):
+        if(x < self.bot_bound[0]):
+            return self.bot_bound[1]
+        elif x > self.top_bound[0]:
+            return self.top_bound[1]
+        if(self.interpol_range == 0):
+            spl = UnivariateSpline(self.values.keys(), self.values.values())
+            spl.set_smoothing_factor(0.5)
+            return spl(x)
