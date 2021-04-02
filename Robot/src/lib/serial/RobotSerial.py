3ff98e3a25a709158c0e35ad3b5e48cf4995b082
meghanlu <lumeghan@gmail.com>
2021-04-01 21:43:26 -0500

    Added encode/decode methods

--- Robot/src/lib/serial/RobotSerial.py
+++ Robot/src/lib/serial/RobotSerial.py
@@ -1,4 +1,5 @@
 import serial
+import json
 
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 
@@ -8,6 +9,8 @@ ser = serial.Serial('/dev/ttyUSB0', 115200)
 # Split encoded serial by comma
 
 d = {1: 000, 2: 000, 3: 000}
+
+terminator_symbol = "@@@"
 
 
 def get_value_from_id(ID):
@@ -39,9 +42,27 @@ def send_serial(message):
 
 def read_serial():
     if ser.in_waiting() > 0:
-        print(ser.read())
+        stuff_from_arduino = ser.read_until(terminator_symbol)
+        decode_json(stuff_from_arduino.replace(terminator_symbol, ''))
+
 
 def run_serial():
+    # TODO: Finalize JSON format for data to send to ESP 32
+    # TODO: Create Python data structure for data to send to ESP 32
     while True:
         read_serial()
         send_serial(input("Input:"))
+
+
+def encode_into_json(data_to_send):
+    # json.dumps() turns python data into JSON-formatted string (to send to ESP 32)
+    json_string = json.dumps(data_to_send)
+    return json_string
+
+
+def decode_json(json_string):
+    # json.loads() turns JSON-formatted string (from ESP 32) into python data
+    received_data = json.loads(json_string)
+    return received_data
+
+
