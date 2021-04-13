2ec4b9af6865bd8bcbf6711de90933989ac54db7
Arjun Pal <anonamause10@gmail.com>
2021-04-12 21:01:06 -0500

    started parser

--- Robot/src/lib/controller_parser/Controller.py
+++ Robot/src/lib/controller_parser/Controller.py
@@ -1,18 +1,17 @@
+from digi.xbee.devices import XBeeDevice
+
 
 class Controller:
 
-    joysticks = [None] * 4
+    def __init__(self):
+        self.device = XBeeDevice("COM1", 9600)
+        self.device.open()
+        self.inputs = [None] * 17
 
-    buttons = [None] * 16
-
-    def read_input(self, input):
-
-        for i in range(len(self.joysticks)):
-            self.joysticks[i]=input[i]
-
-        for i in range(len(self.buttons)):
-            self.buttons[i]=input[i+4]
-
-
-
-    def button_press(self):
+    def readData(self):
+        ip_message = self.device.read_data()
+        self.decodeMsg(ip_message)
+        
+    def decodeMsg(self, message):
+        #HX:  0 HY:  0 A0:-1471 A1: 52 A2:-52 A3:3178 Y:0 B:0 A:0 X:0 THL:0 THR:0 TL:0 TR:0 TL2:0 TR3:0 M:0 ST:0 N:0 E:0 S:0 W:0 SL:0 EN: 0 AE: 0
+        pass
