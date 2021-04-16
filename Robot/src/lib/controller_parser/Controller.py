c574bd539bc4d5049ddef9fdbfed46b1b05da64f
Arjun Pal <anonamause10@gmail.com>
2021-04-15 20:40:53 -0500

    added decoder

--- Robot/src/lib/controller_parser/Controller.py
+++ Robot/src/lib/controller_parser/Controller.py
@@ -14,4 +14,7 @@ class Controller:
         
     def decodeMsg(self, message):
         #HX:  0 HY:  0 A0:-1471 A1: 52 A2:-52 A3:3178 Y:0 B:0 A:0 X:0 THL:0 THR:0 TL:0 TR:0 TL2:0 TR3:0 M:0 ST:0 N:0 E:0 S:0 W:0 SL:0 EN: 0 AE: 0
-        pass
+        messages = message.split()
+        for i in range(17):
+            self.inputs = float(messages[2*i+1].strip(""))
+
